a83964f863ab8c04e72b2122218be681f7a61d17
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-06 16:13:47 +0000

    Refactored the code a little bit

--- game.rb
+++ game.rb
@@ -8,8 +8,25 @@ module MathGame
       @player2 = player2
     end
 
-    def change_player=(new_player)
-      @current_player = new_player
+    def change_player
+      if @current_player === @player1
+        @current_player = @player2
+      else
+        @current_player = @player1
+      end
+    end
+
+    def game_over?
+      if @player1.life == 0
+        puts "#{@player2.name} wins with a score of #{@player2.life}/3"
+        puts "----- GAME OVER -----"
+        puts "Good bye!"
+      elsif @player2.life == 0
+        puts "#{@player1.name} wins with a score of #{@player1.life}/3"
+        puts "----- GAME OVER -----"
+        puts "Good bye!"
+      end
     end
   end
 end
+
--- main.rb
+++ main.rb
@@ -9,13 +9,14 @@ new_game = MathGame::Game.new(p1, p2)
 new_turn = MathGame::Turn.new
 
 
- until p1.life === 0 || p2.life === 0 do
+until p1.life === 0 || p2.life === 0 do
   puts "----- NEW TURN -----"
   new_question = MathGame::Question.new
+
   puts "#{new_game.current_player.name}: #{new_question.print_equation}?"
   print "> "
   answer = $stdin.gets.chomp.to_i
-  # puts new_game.current_player.name
+
   if !new_question.check_equation(answer)
     new_game.current_player.life_lost
     puts new_question.wrong_answer
@@ -24,23 +25,12 @@ new_turn = MathGame::Turn.new
     puts new_question.right_answer
     puts
   end
+
   puts "#{new_game.player1.name} #{new_game.player1.life}/3 vs #{new_game.player2.name} #{new_game.player2.life}/3"
   puts
-  if new_game.current_player === new_game.player1
-    new_game.change_player = new_game.player2
-  else
-    new_game.change_player = new_game.player1
-  end
 
-  if new_game.player1.life == 0
-    puts "#{new_game.player2.name} wins with a score of #{new_game.player2.life}/3"
-    puts "----- GAME OVER -----"
-    puts "Good bye!"
-  elsif new_game.player2.life == 0
-    puts "#{new_game.player1.name} wins with a score of #{new_game.player1.life}/3"
-    puts "----- GAME OVER -----"
-    puts "Good bye!"
-  end
+  new_game.change_player
+  new_game.game_over?
 end
 
 
